a346ea02f3fa1d4793e5b6b7b33165e7615ba983
Fell <76648640+TyFelll@users.noreply.github.com>
2021-04-02 16:54:37 +0200

    Create initiate_BOT_easy.py

---
+++ initiate_BOT_easy.py
@@ -0,0 +1,23 @@
+# Comment créé son propre bot discord | La Base de Base | Level : Very Easy
+
+import discord
+from discord.ext import commands
+# On importe les modules principaux
+
+client = commands.Bot(command_prefix = "*") # Remplacer '*' par ce que vous voulez
+
+@client.event() # C'est un des décorators qui vous permet de activer une fonction
+async def on_ready():
+	"""
+	Cette fonction vous permet d'envoyer un message dans la console 'Ready' quand le bot est prêt
+	Elle permet aussi au bot de lui ajouter un statue 'Watching', sur 'Netflix'
+	"""
+	print("Ready") # Renvoi 'Ready' a la console
+	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name= "Netflix"))
+	# Vous pouvez aussi modifier le 'watching' par playing/streaming/listening
+	# Le name= 'Netflix', est modifiable a votre guise
+
+client.run('TOKEN') # Oublier pas de modifier votre TOKEN par celui de votre bot | Le TOKEN est le code secret de votre bot
+
+# Link : https://discord.com/developers/docs/intro
+# Author : Fell#1679 | Add me one discord for help, I'm Free and Cool
